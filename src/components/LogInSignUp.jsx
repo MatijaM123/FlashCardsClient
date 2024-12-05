654d1d93d9146951e6b08b049b2b0a55830a4bcd
JovanS1212 <2021.09.17@online.devetagimnazija.edu.rs>
2024-12-05 20:23:47 +0100

    bug fix

--- src/components/LogInSignUp.jsx
+++ src/components/LogInSignUp.jsx
@@ -1,35 +1,106 @@
-import React, {useState} from 'react'
-import './LogInSignUp.css'
+import React, { useState } from "react";
+import "./LogInSignUp.css";
+import {Navigate} from "react-router-dom";
 
 const LogInSignUp = () => {
+  function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
 
-const [action,setAction] = useState("Sign Up");
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === " ") {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
 
-const SignUp = ()=>{
-    setAction("Sign up");
+  function checkCookie(cName) {
+    let token = getCookie(cName);
+    if (token !== "") {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-}
-const Login = ()=>{
-    setAction("Login");
-    
-}
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  async function registerUser() {
+    const response = await fetch("http//:localhost:3030/auth/register", {
+      method: "POST", // HTTP method
+      body: JSON.stringify({ username: username, password: password }),
+    });
+    return response.json();
+  }
+
+  const SignUp = () => {
+    registerUser();
+    Login();
+  };
+  const Login = async () => {
+    const response = await fetch("http//:localhost:3030/auth/register", {
+      method: "POST", // HTTP method
+      body: JSON.stringify({ username: username, password: password }),
+    });
+    if (response.ok && response.json() != null) {
+      setCookie("refresh-token", response.json().refreshToken, 5);
+      setCookie("access-token", response.json().accessToken, 5);
+    }
+  };
+  if(checkCookie("access-token")){
+    return <Navigate to='main' replace/>
+  }
   return (
     <div className="container">
-        <div className="headerText">Flash Kartice Dragani</div>
-        <div className="inputs">
-            <div className="input">
-                <input type="text" placeholder='Name'/>
-            </div>
-            <div className="input">
-                <input type="password" placeholder='Password'/>
-            </div>
+      <div className="headerText">Flash Kartice Dragani</div>
+      <div className="inputs">
+        <div className="input">
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+            placeholder="Name"
+          />
         </div>
-        <div className="submit-container">
-            <div className={action==="Login"?"submit gray":"submit"} onClick = {SignUp}>Sign Up</div>
-            <div className={action==="Sign up"?"submit gray":"submit"} onClick = {Login}>Log In</div>
+        <div className="input">
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder="Password"
+          />
         </div>
+      </div>
+      <div className="submit-container">
+        <div className="submit" onClick={SignUp}>
+          Sign Up
+        </div>
+        <div className="submit" onClick={Login}>
+          Log In
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LogInSignUp
+export default LogInSignUp;
--- src/components/Pile.jsx
+++ src/components/Pile.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import "./Pile.css"
+import OpenedPile from './OpenedPile'
 
 const Pile = (props) => {
 
     const OpenPile = () =>{
         //OVDE TREBA POVUCI KARTICE ZA OVAJ SPIL I OTVORITI EKRAN ZA NJEGA
+        
     }
 
     return (
+    <>
     <div className="pile" onClick={OpenPile}>
         <div className="pile-name">{props.name}</div>
         <div className="separation"></div>
         <div className="pile-description">{props.description}</div>
     </div>
+    <OpenedPile visible = "visible"></OpenedPile>
+    </>
   )
 }
 
